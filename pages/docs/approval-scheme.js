fbf925f9d7a0d86a92f5692007a3d96b97911fa9
future3OOO <46719676+future3OOO@users.noreply.github.com>
2023-07-03 20:48:28 +1200

    Update approval-scheme.js

--- pages/docs/approval-scheme.js
+++ pages/docs/approval-scheme.js
@@ -1,6 +1,10 @@
 import styles from './styles.module.css'  
 import Link from 'next/link'
 
+.header-1 {
+    margin-top: 50px;  /* Adjust the value to what suits your design */
+}
+
 export default function ApprovalScheme() {
     return (
         <div className={styles["my-container"]}>
