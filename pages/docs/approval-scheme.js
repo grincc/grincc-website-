1a079e339977521f24bca822fbe5ebc544b09b96
future3OOO <46719676+future3OOO@users.noreply.github.com>
2023-07-03 17:02:44 +1200

    Update approval-scheme.js

--- pages/docs/approval-scheme.js
+++ pages/docs/approval-scheme.js
@@ -9,42 +9,53 @@ export default function ApprovalScheme() {
                 As all members of the GRIN community council all have owner roles on this repository, the following rules for approvals of pull-requests(PR) are suggested:
             </p>
 
-            <table className="table border-green-200 mt-8 mx-auto  dark:bg-black  ">
-                <thead   >
-                    <tr >
-                        <th   >Repository</th>
-                        <th>Required members of CC for PR</th>
-                        <th>notes</th>
-                    </tr>
-                </thead>
-                <tbody >
-                    <tr>
-                        <td><Link href="https://github.com/grincc/agenda"><a>agenda</a></Link> </td>
-                         <td className="text-center">1</td>
-                         <td>CC meeting notes, etc</td>
-                    </tr>
-                    <tr>
-                        <td><Link href="https://github.com/grincc/hub"><a>hub</a></Link> </td>
-                         <td className="text-center">1</td>
-                         <td>code, tools, sites, memes, marketing, art</td>
-                    </tr>
-                    <tr>
-                        <td><Link href="https://github.com/grincc/security"><a>security</a></Link> </td>
-                        <td  className="text-center">4</td>
-                        <td>signing, verifying</td>
-                    </tr>
-                    <tr>
-                        <td><Link href="https://github.com/grincc/finance"><a>finance</a></Link> </td>
-                        <td  className="text-center">4</td>
-                        <td>public CC fund documentation</td>
-                    </tr>
-                    <tr>
-                        <td><Link href="https://github.com/grincc/docs"><a>docs</a></Link> </td>
-                        <td  className="text-center">2</td>
-                        <td>CC related documents</td>
-                    </tr>
-                </tbody>
-            </table>
+                .border-frame {
+    border: 1px solid green;
+}
+
+.border-frame td,
+.border-frame th {
+    border: 1px solid green;
+}
+
+
+            <table className="table border-green-200 mt-8 mx-auto dark:bg-black border-frame">
+    <thead>
+        <tr>
+            <th>Repository</th>
+            <th>Required members of CC for PR</th>
+            <th>notes</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td><Link href="https://github.com/grincc/agenda"><a>agenda</a></Link></td>
+            <td className="text-center">1</td>
+            <td>CC meeting notes, etc</td>
+        </tr>
+        <tr>
+            <td><Link href="https://github.com/grincc/hub"><a>hub</a></Link></td>
+            <td className="text-center">1</td>
+            <td>code, tools, sites, memes, marketing, art</td>
+        </tr>
+        <tr>
+            <td><Link href="https://github.com/grincc/security"><a>security</a></Link></td>
+            <td className="text-center">4</td>
+            <td>signing, verifying</td>
+        </tr>
+        <tr>
+            <td><Link href="https://github.com/grincc/finance"><a>finance</a></Link></td>
+            <td className="text-center">4</td>
+            <td>public CC fund documentation</td>
+        </tr>
+        <tr>
+            <td><Link href="https://github.com/grincc/docs"><a>docs</a></Link></td>
+            <td className="text-center">2</td>
+            <td>CC related documents</td>
+        </tr>
+    </tbody>
+</table>
+
 
 
         </div>
