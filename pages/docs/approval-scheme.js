c59b5233ce04f7f19d600fe3f64a6c7e675411f8
future3OOO <46719676+future3OOO@users.noreply.github.com>
2023-07-03 17:21:53 +1200

    Update approval-scheme.js

--- pages/docs/approval-scheme.js
+++ pages/docs/approval-scheme.js
@@ -1,6 +1,6 @@
-import Link from 'next/link'
-import './styles.css';
+import './styles.css';  // Path to your CSS file
 
+import Link from 'next/link'
 
 export default function ApprovalScheme() {
     return (
